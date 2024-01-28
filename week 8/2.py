61f09b6697d6ec48d353fab7cd08e309a2be6151
Sushan2428 <153786481+Sushan2428@users.noreply.github.com>
2024-01-28 17:17:16 +0545

    Add files via upload

---
+++ week 8/2.py
@@ -0,0 +1,12 @@
+import sys
+
+try:
+    with open(sys.argv[1], 'r') as first, open(sys.argv[2], 'r') as second:
+        if first.read() == second.read():
+            print('Files are the same.')
+        else:
+            print('Files are different')
+except IndexError:
+    print(f'{sys.argv[0]}: Missing required argument(s).')
+except FileNotFoundError:
+    print(f'{sys.argv[0]}: Error opening files to compare.')
---
+++ week 8/3.py
@@ -0,0 +1,14 @@
+import sys
+
+try:
+    file_name = sys.argv[2]
+    pattern = sys.argv[1]
+
+    with open(file_name, 'r') as infile:
+        for line in infile:
+            if pattern in line:
+                print(line, end='')
+except IndexError:
+    print(f'{sys.argv[0]}: Missing required argument.')
+except FileNotFoundError:
+    print(f'{sys.argv[0]}: Cannot open "{sys.argv[2]}"')
